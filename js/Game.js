755fc23748ca436795d860eebbb1d075053fa234
DevilGaming <77532381+Dhanigaming@users.noreply.github.com>
2021-05-15 16:37:50 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,54 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Start the race",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!== undifined){
+      var displayPosition = 130;
+
+      for(var plr in allPlayers){
+       if(plr === "player" + player.index){
+         fill("blue");
+       }else{
+         fill("black");
+       }
+      }
+
+      displayPosition += 20;
+      textSize(14);
+      text(allPlayers[plr].name + ":" + allPlayers[plr].distance , 120 , displayPosition);
+    }
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 50;
+      player.update();
+  
+    }
+  }
+  
+}
